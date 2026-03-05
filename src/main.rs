aa9a46c4ca3cd04d6fb333998cb3f75f30b225fc
moyaqoob <moyaqoob28@gmail.com>
2026-03-05 08:55:44 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+
+async fn greet(req:HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("
+    World");
+    HttpResponse::Ok().body(format!("hello {}",name))
+}
+
+// #[tokio::main]
+fn main() -> std::io::Result<()>{
+    let body  = async move {
+        HttpServer::new(||{
+            App::new()
+                .route("/",web::get().to(greet) )  
+                .route("/{name}",web::get().to(greet) )  
+        })
+        .bind("127.0.0.1:8080")?
+        .run()
+    }
+
+    
+    
+}
